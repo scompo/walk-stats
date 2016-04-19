4c52b07c26b9d5c014324a4cd7f61d20eaed2aa0
Mauro Scomparin <scompo@gmail.com>
2016-04-19 23:28:58 +0200

    removed useless failing test from ApplicationTest

--- src/test/java/com/github/scompo/walkstats/ApplicationTest.java
+++ src/test/java/com/github/scompo/walkstats/ApplicationTest.java
@@ -1,17 +1,10 @@
 package com.github.scompo.walkstats;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class ApplicationTest {
-
-	@Test
-	public void failingTest() {
-
-		fail("this fails!");
-	}
 
 	@Test
 	public void passingTest() {
