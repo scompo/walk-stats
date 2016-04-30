78253163cda5c72e0c0e0c31d6f18d29aeb669ad
Mauro Scomparin <scompo@gmail.com>
2016-04-30 17:48:09 +0200

    Simplified matches in CompareToBigDecimalMatcher

--- src/main/java/com/github/scompo/walkstats/Position.java
+++ src/main/java/com/github/scompo/walkstats/Position.java
@@ -1,52 +1,28 @@
 package com.github.scompo.walkstats;
 
-import java.time.Instant;
+/**
+ * Position API definition.
+ */
+public interface Position {
 
-public class Position implements Timed {
+	/**
+	 * Returns the {@link Longitude} of a position.
+	 * 
+	 * @return the {@link Longitude} of a position.
+	 */
+	Longitude getLongitude();
 
-	private final Latitude latitude;
+	/**
+	 * Returns the {@link Latitude} of a position.
+	 * 
+	 * @return the {@link Latitude} of a position.
+	 */
+	Latitude getLatitude();
 
-	private final Longitude longitude;
-
-	private final Altitude altitude;
-
-	private final Instant time;
-	
-	public static Position createPosition(Latitude latitude,
-			Longitude longitude, Altitude altitude, Instant time) {
-		
-		return new Position(latitude, longitude, altitude, time);
-	}
-
-	private Position(Latitude latitude, Longitude longitude, Altitude altitude,
-			Instant time) {
-
-		this.latitude = latitude;
-		this.longitude = longitude;
-		this.altitude = altitude;
-		this.time = time;
-	}
-
-	public Latitude getLatitude() {
-		return latitude;
-	}
-
-	public Longitude getLongitude() {
-		return longitude;
-	}
-
-	public Altitude getAltitude() {
-		return altitude;
-	}
-
-	@Override
-	public Instant getTime() {
-		return time;
-	}
-
-	@Override
-	public String toString() {
-		return "Position [latitude=" + latitude + ", longitude=" + longitude
-				+ ", altitude=" + altitude + ", time=" + time + "]";
-	}
+	/**
+	 * Returns the {@link Altitude} of a position.
+	 * 
+	 * @return the {@link Altitude} of a position.
+	 */
+	Altitude getAltitude();
 }
